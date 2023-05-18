d8fbe9fe5fa2572b1b1940a7d1b4c9d983fc8a4a
shrouk hakim <shroukhakim222@gmail.com>
2023-05-18 08:49:09 -0700

    compiler

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,10 +13,10 @@ int main(void)
 	long long int lli;
 	float f;
 
-	printf("Size of a char: %zu byte(s)\n", c);
-	printf("Size of an int: %zu byte(s)\n", i);
-	printf("Size of a long int: %zu  byte(s)\n", li);
-	printf("Size of a long long int: %zu byte(s)\n", lli);
-	printf("Size of a float: %zu byte(s)\n", f);
+	printf("Size of a char: %zu byte(s)\n", sizeof(c));
+	printf("Size of an int: %zu byte(s)\n", sizeof(i));
+	printf("Size of a long int: %zu  byte(s)\n", sizeof(li));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(lli));
+	printf("Size of a float: %zu byte(s)\n", sizeof(f));
 	return (0);
 }
