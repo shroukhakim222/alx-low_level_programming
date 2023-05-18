7e50b4f591e3ff0cb9618a1c48b1bbf800cddf63
shrouk hakim <shroukhakim222@gmail.com>
2023-05-18 11:32:32 -0700

    compilar

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,65 +8,19 @@
 
 int main(void)
 {
-	/*and that piece of art is useful" - Dora Korpar, 2015-10-19*/
-	putchar('a');
-	putchar('n');
-	putchar('d');
-	putchar(' ');
-	putchar('t');
-	putchar('h');
-	putchar('a');
-	putchar('t');
-	putchar(' ');
-	putchar('p');
-	putchar('i');
-	putchar('e');
-	putchar('c');
-	putchar('e');
-	putchar(' ');
-	putchar('o');
-	putchar('f');
-	putchar(' ');
-	putchar('a');
-	putchar('r');
-	putchar('t');
-	putchar(' ');
-	putchar('i');
-	putchar('s');
-	putchar(' ');
-	putchar('u');
-	putchar('s');
-	putchar('e');
-	putchar('f');
-	putchar('u');
-	putchar('l');
-	putchat('\"');
-	/*and that piece of art is useful" - Dora Korpar, 2015-10-19*/
-	putchar(' ');
-	putchar('-');
-	putchar(' ');
-	putchar('D');
-	putchar('o');
-	putchar('r');
-	putchar('a');
-	putchar(' ');
-	putchar('K');
-	putchar('o');
-	putchar('r');
-	putchar('p');
-	putchar('a');
-	putchar('r');
-	putchar(',');
-	putchar(' ');
-	putchar('2');
-	putchar('0');
-	putchar('1');
-	putchar('5');
-	putchar('-');
-	putchar('1');
-	putchar('0');
-	putchar('-');
-	putchar('1');
-	putchar('9');
+	char st[] = {'a', 'n', 'd', ' ', 't', 'h', 'a', 't', ' ',
+		'p', 'i', 'e', 'c', 'e', ' ', 'o', 'f',
+		' ', 'a', 'r', 't', ' ', 'i', 's', ' ',
+		'u', 's', 'e', 'f', 'u', 'l', '"', ' ',
+		'-', ' ', 'D', 'o', 'r', 'a', ' ', 'K',
+		'o', 'r', 'p', 'a', 'r', ',', ' ', '2', '0',
+		'1', '5', '-', '1', '0', '-', '1', '9'};
+	int len = sizeof(st) - 1;
+	int i;
+
+	for (i = 0; i < len; i++)
+	{
+		putchar(st[i]);
+	}
 	return (1);
 }
