ec92e65c4adc67f8809e0c524c9565fa7efee213
shrouk hakim <shroukhakim222@gmail.com>
2023-05-19 05:01:46 -0700

    varible

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = 48; i < 57; i++)
+	{
+		for (j = i + 1; j < 58; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if (i != 56)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
