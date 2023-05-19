0d9c5b0ae6501db6d44049b42845a02d3db5042a
shrouk hakim <shroukhakim222@gmail.com>
2023-05-19 04:39:11 -0700

    varible

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - print number of base 10
+ *
+ * Return: always 0
+ */
+
+int main(void)
+{
+	int i;
+	
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+		if (i < 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+		return (0);
+	}
+}
