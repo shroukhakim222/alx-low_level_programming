a5afa1bf5c7e711b32e10fc0378f672e47ecffef
shrouk hakim <shroukhakim222@gmail.com>
2023-05-19 04:15:16 -0700

    variable

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
