32867f175ed570cd7dce38cfb9a78ba5b1b54650
shrouk hakim <shroukhakim222@gmail.com>
2023-05-20 00:20:55 -0700

    variable

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i, j, y, x;
+
+	for (i = 48; i < 58; i++)
+	{
+		for (j = 48; j < 57; j++)
+		{
+			for (y = 48; y < 58; y++)
+			{
+				for (x = 48; x < 58; x++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(y);
+					putchar(x);
+					if (i != 57 && j != 56)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
