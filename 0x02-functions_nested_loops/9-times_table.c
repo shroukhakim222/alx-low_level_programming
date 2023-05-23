bdcd8c3cc8a98e5b1f04efe61dd2170de63917bd
shrouk hakim <shroukhakim222@gmail.com>
2023-05-23 10:44:17 -0700

    nested loop

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -20,11 +20,14 @@ void times_table(void)
 			mul = i * j;
 			fst = mul / 10;
 			lst = mul % 10;
-			_putchar(' ');
-			if (fst == 0)
+			if (j == 0 || (j == 0 && i == 0))
+			{
 				_putchar(' ');
-			else
-				_putchar(fst + '0');
+				if (fst == 0)
+					_putchar(' ');
+				else
+					_putchar(fst + '0');
+			}
 			_putchar(lst + '0');
 			if (j == 9)
 				break;
