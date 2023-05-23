23e93745565402e4b76347822e059672f74cda67
shrouk hakim <shroukhakim222@gmail.com>
2023-05-23 02:25:56 -0700

    nested loop

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -13,7 +13,7 @@ void jack_bauer(void)
 
 	for (i = 48; i < 51; i++)
 	{
-		for (j = 48; j < 52; j++)
+		for (j = 48; j < 58; j++)
 		{
 			for (x = 48; x < 54; x++)
 			{
@@ -25,6 +25,8 @@ void jack_bauer(void)
 					_putchar(x);
 					_putchar(y);
 					_putchar('\n');
+					if (i == 50 && j == 51 && x == 53 && y == 57)
+						break;
 				}
 			}
 		}
