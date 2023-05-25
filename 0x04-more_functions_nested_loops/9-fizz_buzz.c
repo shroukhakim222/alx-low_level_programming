60ac464d7dad3a65aa191699303800c372355581
shrouk hakim <shroukhakim222@gmail.com>
2023-05-25 04:44:00 -0700

    more function

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -7,3 +7,23 @@
  * Return: printed game from 0 to 100
  */
 
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else
+			printf("%d", i);
+		if (i == 100)
+			break;
+		printf(" ");
+	}
+	printf("\n");
+}
