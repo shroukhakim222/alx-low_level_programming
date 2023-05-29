7576eea705e157b9c1bda064f0c7f873397cce0a
shrouk hakim <shroukhakim222@gmail.com>
2023-05-29 01:22:39 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _puts - print string followed by new line
+ * @str: pointer to string
+ * Return: string followed by new line in stdout
+ */
+
+void _puts(char *str)
+{
+	printf("%c\n", str);
+}
