2d9a9700cddb24208aaa1438931af16f15d9ca22
shrouk hakim <shroukhakim222@gmail.com>
2023-05-29 03:50:01 -0700

    pointer

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -20,7 +20,7 @@ char *_strcpy(char *dest, char *src)
 		len1++;
 	while (src[len2] != '\0')
 		len2++;
-	for (i = len1; i < (len1 + len2) - 1; i++)
+	for (i = len1; i < (len1 + len2); i++)
 	{
 		dest[i] = src[y];
 		y++;
