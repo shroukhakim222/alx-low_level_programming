3f13b4fcae6e0266abf6729ff707557f0a158db2
shrouk hakim <shroukhakim222@gmail.com>
2023-05-29 03:42:59 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *_strcpy - function add two string
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: pointer to modified string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len1 = 0;
+	int len2 = 0;
+	int i;
+	int y = 0;
+
+	while (dest[len1] != '\0')
+		len1++;
+	while (src[len2] != '\0')
+		len2++;
+	for (i = len1; i < len1 + len2 -1; i++)
+	{
+		dest[i] = src[y];
+		y++;
+	}
+	return (dest);
+}
