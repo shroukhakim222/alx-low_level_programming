f633879eb0e498b70eaba2e48faca7bfb1da6ffa
shrouk hakim <shroukhakim222@gmail.com>
2023-05-29 01:57:57 -0700

    pointer

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * rev_string - function reverse string
+ * @s: string pointer
+ * Return: reversed string
+ */
+
+void rev_string(char *s)
+{
+	int i, n;
+	int len = 0;
+	char *rev;
+
+	while (s[len] != '\0')
+		len++;
+	n = len - 1;
+	for (i = 0; i < len; i++)
+	{
+		*rev = s[n];
+		n--;
+	}
+	return (rev);
+}
