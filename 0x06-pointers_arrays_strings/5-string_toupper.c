afb745eb261dbe47bd5dd94f99df55e95947b1f5
shrouk hakim <shroukhakim222@gmail.com>
2023-05-31 10:29:09 -0700

    more pointer

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -10,11 +10,11 @@
 
 char *string_toupper(char *s)
 {
-	int i;
+	int i = 0;
 
 	while (s[i] != '\0')
 	{
-		if (s[i] >= 97 && s[i] <= 122)
+		if (s[i] >= 'a' && s[i] <= 'z')
 			s[i] -= 32;
 		i++;
 	}
