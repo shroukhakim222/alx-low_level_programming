da17829c1e7c86e3068d572ab5d240b5cb764745
shrouk hakim <shroukhakim222@gmail.com>
2023-05-31 12:49:22 -0700

    more pointer

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *cap_string - capitalizes all words of a string
+ * @s: pointer to string
+ *
+ * Return: capitalized array
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		while (!(s[i] >= 'a' && s[i] <= 'z'))
+			i++;
+		if (s[i - 1] == ' '
+				|| s[i - 1] == '\t'
+				|| s[i - 1] == '\n'
+				|| s[i - 1] == ','
+				|| s[i - 1] == ';'
+				|| s[i - 1] == '.'
+				|| s[i - 1] == '!'
+				|| s[i - 1] == '?'
+				|| s[i - 1] == '"'
+				|| s[i - 1] == '('
+				|| s[i - 1] == ')'
+				|| s[i - 1] == '{'
+				|| s[i - 1] == '}')
+			s[i] -= 32;
+		i++;
+	}
+	return (s);
+}
