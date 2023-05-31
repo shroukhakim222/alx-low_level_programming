1c9f693ecfa954086cee92aeb24e292688acff1b
shrouk hakim <shroukhakim222@gmail.com>
2023-05-31 09:00:50 -0700

    more pointer

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -12,4 +12,23 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
+	int i, j;
+	int len = 0;
+	int y;
+
+	while (src[len] != '\0')
+		len++;
+	y = (n > len) ? len : n;
+	i = 0;
+	while (dest[i] != '\0' && i < y)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	if (n > len)
+	{
+		for (j = len; j < n; j++)
+			dest[j] = '\0';
+	}
+	return (dest);
 }
