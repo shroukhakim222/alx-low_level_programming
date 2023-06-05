33fd7ec4eddda545083d97d05c75b0bcffe0e0a7
shrouk hakim <shroukhakim222@gmail.com>
2023-06-05 11:39:40 -0700

    even more pointer

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -12,16 +12,12 @@ void print_chessboard(char (*a)[8])
 {
 	int i, j;
 
-	i = 0;
-	j = 0;
-	while (a[i][j] != '\0')
+	for (i = 0; i < 8; i++)
 	{
 		for (j = 0; j < 8; j++)
 		{
-			if (a[i][j] != ' ')
-				_putchar(a[i][j]);
+			_putchar(a[i][j]);
 		}
 		_putchar('\n');
-		i++;
 	}
 }
