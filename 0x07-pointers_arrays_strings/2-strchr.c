98d957a9e8eb11e6d78f6d8fc771c6cc2ec3a26d
shrouk hakim <shroukhakim222@gmail.com>
2023-06-06 01:57:27 -0700

    even more pointer

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -14,13 +14,11 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	while (s[i] != '\0')
+	for (; s[i] >= '\0'; i++)
 	{
 		if (s[i] == c)
 			return (&s[i]);
-		else
-			return (0);
-		i++;
 	}
 	return (0);
+
 }
