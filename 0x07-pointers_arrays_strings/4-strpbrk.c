2984a3e6bd1f77a85fdaa4515280b32bfeced4cc
shrouk hakim <shroukhakim222@gmail.com>
2023-06-06 02:09:19 -0700

    even more pointer

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -14,13 +14,14 @@ char *_strpbrk(char *s, char *accept)
 {
 	int i, j;
 
-	for (i = 0; s[i] >= '\0'; i++)
+	for (i = 0; s[i] >= '\0';)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 				return (&s[i]);
 		}
+		i++;
 	}
-	return ('\0');
+	return (0);
 }
