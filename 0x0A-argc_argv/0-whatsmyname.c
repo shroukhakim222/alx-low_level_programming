4ffb25723d196d5241834c8bb7f61e765b6f4709
shrouk hakim <shroukhakim222@gmail.com>
2023-06-09 06:40:12 -0700

    command line argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/*
+ * main - print program name
+ *
+ * Return: always.
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
