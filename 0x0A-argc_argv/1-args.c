d5a705b8d6e5b82d3a79b6b9264469072e40ca2d
shrouk hakim <shroukhakim222@gmail.com>
2023-06-09 07:05:14 -0700

    command line argument

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints the number of arguments
+ * @argc: command count
+ * @argv: command array
+ *
+ * Return: always 0
+ */
+
+int main (int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc - 1);
+	return (0);
+}
