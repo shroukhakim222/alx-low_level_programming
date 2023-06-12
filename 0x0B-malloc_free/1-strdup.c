38f4e111b3c7d6d2e79d2fd08720ae8e16e995a5
shrouk hakim <shroukhakim222@gmail.com>
2023-06-12 13:30:09 -0700

    recursion

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strdup -  returns a pointer to a newly allocated space
+ * @str: pointer to array
+ *
+ * Return: pointer to a newly allocated space in memory
+ */
+
+char *_strdup(char *str)
+{
+	char *ptr;
+	int i;
+	int size;
+
+	size = sizeof(str);
+	ptr = malloc(sizeof(char) * size + 1);
+	if (ptr == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		ptr[i] = str[i];
+	ptr[i] = '\0';
+	return (ptr);
+}
