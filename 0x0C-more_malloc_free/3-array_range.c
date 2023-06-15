4fa80e75dd172e1111307153c1746bb7a4fdd013
shrouk hakim <shroukhakim222@gmail.com>
2023-06-15 03:03:02 -0700

    more malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -19,7 +19,7 @@ int *array_range(int min, int max)
 	arr = malloc(sizeof(int) * (max - min + 1));
 	if (arr == NULL)
 		return (NULL);
-	for (i = min; i <= max; i++)
-		arr[i] = i;
+	for (i = 0; min <= max; i++)
+		arr[i] = min++;
 	return (arr);
 }
