bd03a27163d907c890fd90f50451c7a08f74663f
shrouk hakim <shroukhakim222@gmail.com>
2023-06-19 12:01:35 -0700

    structure

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -32,7 +32,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog);
-		free(new_dog->owner);
+		free(new_dog->name);
 		return (NULL);
 	}
 	_strcpy(new_dog->name, name);
