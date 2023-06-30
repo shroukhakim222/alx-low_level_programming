7c4dcb750f240f43fe4d53a6bb3a2af85472346f
shrouk hakim <shroukhakim222@gmail.com>
2023-06-30 13:13:50 -0700

    singly linked list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - rees a list_t list.
+ * @head: pointer to first node
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *next;
+
+	while (head != NULL)
+	{
+		next = head;
+		free(head);
+		head = next;
+	}
+}
