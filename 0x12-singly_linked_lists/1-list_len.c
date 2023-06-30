77544704a9784ea0d3f0613f563faf5ad474455f
shrouk hakim <shroukhakim222@gmail.com>
2023-06-30 10:40:31 -0700

    single linked list

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * list_len - number of elements in a linked list_t list
+ * @h: pointer to the head
+ * Return: number of element
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t count;
+	const list *ptr;
+
+	ptr = h;
+	while (ptr != NULL)
+	{
+		count++;
+		ptr = ptr->next;
+	}
+	return (count);
+}
